915e15f002ad2dfde2038f6977399c69e8e4a5a4
Adam <adam.kimmerer@gm.com>
2014-12-04 13:40:51 -0500

    jQuery menu scroll
    Added jQuery script to dynamically add anchor classes to posts.

--- js/main.js
+++ js/main.js
@@ -12,3 +12,59 @@ $(function() {
     }
   });
 });
+
+
+$(document).ready(function() {
+  i = 0;
+  $(".posts>li").each(function() {
+    i++;
+    $(this).attr('id', "foo" + i);
+    $(this).addClass("height");
+  });
+  i = 0;
+  $(".titles>li>a").each(function() {
+    i++
+    $(this).attr('href', "#foo" + i);
+  });
+});
+
+$(function () {
+    $('a').on('click', function (e) {
+        // e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+            console.log(target);
+
+        $('#postCont').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {});
+        
+        console.log(window.location);
+
+        return false;
+    });
+});
+
+$(document).ready(function() {
+  $("#postCont").scroll(function(){
+    if ($(this).scrollTop() > 200) {
+        $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+});
+
+$(function () {
+    $('.scrollToTop').on('click', function (e) {
+
+        $('#postCont').stop().animate({
+            'scrollTop': 0
+        }, 700, 'swing', function () {});
+        
+        console.log(window.location);
+
+        return false;
+    });
+});
